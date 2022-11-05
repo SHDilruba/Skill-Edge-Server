b6c6ff31e29ed8b1c63f77671080d688282d7101
unknown <hasinadilruba2021@gmail.com>
2022-11-05 03:16:17 +0100

    server is running

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+const courses = require('./data.json');
+
+app.get ('/', (req, res) =>{
+  res.send('server is running')
+});
